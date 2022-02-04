7c428dcfa13073746b4e264642d2feb91cf0417e
Agustin Lobato <agustinlobato82@gmail.com>
2022-02-03 21:54:34 -0300

    Servidores con Express

--- src/Files/clase.js
+++ src/Files/clase.js
@@ -1,85 +1,85 @@
-const fs = require('fs')
-const express = require('express')
-const http = require ('http')
+ const fs = require('fs')
+ const express = require('express')
+ const http = require ('http')
 
-class Contenedor{
-    constructor(archivo){
-        this.product = [{title:archivo.title,price:archivo.price,thumbnail:archivo.thumbnail}]
+ class Contenedor{
+     constructor(archivo){
+         this.product = [{title:archivo.title,price:archivo.price,thumbnail:archivo.thumbnail}]
     }
 
-    async save (title,price,thumbnail){
-        this.product = {title:title,price:price,thumbnail:thumbnail}
-        try{
-            let products = await fs.promises.readFile('./products.json', 'utf-8')
-            let file = [] 
-            if(products){
-                file = JSON.parse(products)
+     async save (title,price,thumbnail){
+         this.product = {title:title,price:price,thumbnail:thumbnail}
+         try{
+             let products = await fs.promises.readFile('./products.json', 'utf-8')
+             let file = [] 
+             if(products){
+                 file = JSON.parse(products)
             }
-            let files = [...file]
-            const viajes = {
-                title: title,
-                price: price,
-                thumbnail : thumbnail,
-                id: files.length > 0 ? files.pop().id + 1 : 1
+             let files = [...file]
+             const viajes = {
+                 title: title,
+                 price: price,
+                 thumbnail : thumbnail,
+                 id: files.length > 0 ? files.pop().id + 1 : 1
             };
-            let archivo = [...file, viajes]
-            console.log("id:",viajes.id, "file uploaded successfully")
-            await fs.promises.writeFile('./products.json', JSON.stringify(archivo));
+             let archivo = [...file, viajes]
+             console.log("id:",viajes.id, "file uploaded successfully")
+             await fs.promises.writeFile('./products.json', JSON.stringify(archivo));
         }catch(error){
-            console.log(error)
+             console.log(error)
         }
     }
-    async getById(id){
-        try{
-            let products = await fs.promises.readFile('./products.json', 'utf-8')
-            let file = [] 
-            if(products){
-                file = JSON.parse(products) 
+     async getById(id){
+         try{
+             let products = await fs.promises.readFile('./products.json', 'utf-8')
+             let file = [] 
+             if(products){
+                 file = JSON.parse(products) 
             }
-            let files = [...file]
-            let filtrado = files.filter(idHijo => idHijo.id === id)
-            console.log(filtrado.length > 0 ? filtrado : "not find id")
+             let files = [...file]
+             let filtrado = files.filter(idHijo => idHijo.id === id)
+             console.log(filtrado.length > 0 ? filtrado : "not find id")
         }catch(error){
-            console.log(error)
+             console.log(error)
         }
     }
-    async getAll(){
-        try{
-            let products = await fs.promises.readFile('./products.json', 'utf-8')
-            let file = [] 
-            if(products){
-                file = JSON.parse(products) 
+     async getAll(){
+         try{
+             let products = await fs.promises.readFile('./products.json', 'utf-8')
+             let file = [] 
+             if(products){
+                 file = JSON.parse(products) 
             }  
-            console.log(file.length > 0 ? file : "no products in the database")
+             console.log(file.length > 0 ? file : "no products in the database")
         }catch(error){
-            console.log(error)
+             console.log(error)
         }
     }
-    async deleteById(id){
-        try{
-            let products = await fs.promises.readFile('./products.json', 'utf-8')
-            let file = []
-            if(products){
-                file = JSON.parse(products) 
+     async deleteById(id){
+         try{
+             let products = await fs.promises.readFile('./products.json', 'utf-8')
+             let file = []
+             if(products){
+                 file = JSON.parse(products) 
             }
-            let files = [...file]
-            let filtrado = files.filter(idHijo => idHijo.id !== id)
-            if(files.some(idHijo => idHijo.id === id)){
-                console.log(`the product id: ${id} remove to the array list`)
-                await fs.promises.writeFile('./products.json', JSON.stringify(filtrado))
+             let files = [...file]
+             let filtrado = files.filter(idHijo => idHijo.id !== id)
+             if(files.some(idHijo => idHijo.id === id)){
+                 console.log(`the product id: ${id} remove to the array list`)
+                 await fs.promises.writeFile('./products.json', JSON.stringify(filtrado))
             }else{
-                console.log("the product does not exist with this id:", id)
+                 console.log("the product does not exist with this id:", id)
             }
         }catch(error){
-            console.log(error)
+             console.log(error)
         }
     }
-    async deleteAll(){
-        try{
-            await fs.promises.writeFile('./products.json', JSON.stringify([]))
+     async deleteAll(){
+         try{
+             await fs.promises.writeFile('./products.json', JSON.stringify([]))
         }catch(error){
-            console.log(error)
+             console.log(error)
         }
     }
 }
-module.exports = Contenedor
+ module.exports = Contenedor
--- src/Files/test.js
+++ src/Files/test.js
@@ -1,11 +1,11 @@
-const { send } = require("process")
-const clase = require("./clase")
+ const { send } = require("process")
+ const clase = require("./clase")
 
-let person = new clase("hola")
+ let person = new clase("hola")
  person.save("Viaje a Uruguay", 2000, "src/Images/paisajes/uruguay.jpg")
  person.getById(8)
  person.getAll()
-//   person.deleteById(1)
-// person.deleteAll()
+ // person.deleteById(1)
+ // person.deleteAll()
 
  console.log(person.product)
